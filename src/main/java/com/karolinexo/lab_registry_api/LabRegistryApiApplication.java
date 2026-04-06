6fd841751f0f75b0b40c10be09aa381eb75b3369
karolinexo <mariakarolinexo@gmail.com>
2026-04-06 11:06:49 -0300

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/karolinexo/lab_registry_api/LabRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package com.karolinexo.lab_registry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRegistryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRegistryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karolinexo/lab_registry_api/LabRegistryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karolinexo.lab_registry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRegistryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
