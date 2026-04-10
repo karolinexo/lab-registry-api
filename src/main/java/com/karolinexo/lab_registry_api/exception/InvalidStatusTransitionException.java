1abf673c999ab0782f3e517d5ab770f9b77a0d33
karolinexo <mariakarolinexo@gmail.com>
2026-04-10 11:45:25 -0300

    fix: add space between words in status transition error message

--- src/main/java/com/karolinexo/lab_registry_api/exception/InvalidStatusTransitionException.java
+++ src/main/java/com/karolinexo/lab_registry_api/exception/InvalidStatusTransitionException.java
@@ -4,6 +4,6 @@ import com.karolinexo.lab_registry_api.enums.Status;
 
 public class InvalidStatusTransitionException  extends RuntimeException {
     public InvalidStatusTransitionException(Status current, Status next) {
-        super("Invalid status transition from" + current + " to " + next);
+        super("Invalid status transition from " + current + " to " + next);
     }
 }
